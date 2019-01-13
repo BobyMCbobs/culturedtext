0aa35b35ff667ffe229fd0e101f588cf8c708be3
Caleb Woodbine <calebwoodbine.public@gmail.com>
2019-01-13 20:32:34 +1300

    UPDATED: README.md, index.js, package.json
    Added:
    - surreal function
    - version information in module

--- README.md
+++ README.md
@@ -13,9 +13,17 @@ var culture = require('culturedtext');
 
 console.log(culture.oddcase("How do I import a library in nodejs?"));
 console.log(culture.aethstetic("nvm I found it."));
+console.log(culture.surreal("surreal anyone?"));
 ```
 
 | Function | Description | Example output |
 | - | - | - |
 | oddcase | adjusts the case of each letter randomly | My wEBservER IS WriTtEn in WHIteSpaCe | 
 | aethstetic | lower cases text and adds one space between each character of input | a e t h s t e t i c |
+| surreal | either applies random accents to each applicable character, uses aethstetic (minus double spacing), or applies uppercase on all characters with a trailing fullstop | ýõür téxt héré; y o u r  t e x t  h e r e; YOUR TEXT HERE. |
+
+## License
+Copyright 2018 BobyMCbobs.  
+This project is licensed under the [GPL-3.0](http://www.gnu.org/licenses/gpl-3.0.html) and is [Free Software](https://www.gnu.org/philosophy/free-sw.en.html).  
+This program comes with absolutely no warranty.  
+
--- index.js
+++ index.js
@@ -17,7 +17,12 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
 function oddcase(input) {
+ // masH thOse CaSES
 	var output = "";
 	for (var i in input) {
 		output += Math.round(Math.random() * 1) === 0 ? input[i].toUpperCase() : input[i];
@@ -26,6 +31,7 @@ function oddcase(input) {
 }
 
 function aethstetic(input) {
+  // spacing is  s o  a e t h s t e t i c
 	var output = "";
 	for (var i in input) {
 		output += `${input[i]} `;
@@ -33,5 +39,48 @@ function aethstetic(input) {
 	return output.toLowerCase().trim();
 }
 
+function surreal(input) {
+  // r/surrealmemes style
+  var output = "";
+  const chars = {
+    "a": ["à", "á", "â", "ã", "ä", "å", "æ"],
+    "c": ["ç"],
+    "d": ["ð"],
+    "e": ["è", "é", "ê", "ë"],
+    "f": ["ƒ"],
+    "i": ["ì", "í", "î", "ï"],
+    "n": ["ñ"],
+    "o": ["ò", "ó", "ô", "õ", "ö", "ø", "œ"],
+    "s": ["š"],
+    "u": ["ù", "ú", "û", "ü"],
+    "y": ["ý", "ÿ"]
+  }
+
+  switch(["accents", "uppercase", "surrealaesthetic"].random()) {
+    case "accents":
+      for (var i in input) {
+        var adjustment = chars[input[i].toLowerCase()],
+          charIsUpper = input[i] === input[i].toUpperCase();
+        if (adjustment === undefined) adjustment = input[i];
+        else {
+          adjustment = adjustment.random();
+        }
+        output += charIsUpper === true ? adjustment.toUpperCase() : adjustment;
+      }
+      break;
+    case "uppercase":
+      output = `${input.toUpperCase()}.`;
+      break;
+    case "surrealaesthetic":
+      var re = new RegExp(/  /g);
+      output = aethstetic(input).toLowerCase();
+      output = output.replace(re, " ");
+      break;
+  }
+  return output;
+}
+
 exports.oddcase = oddcase;
 exports.aethstetic = aethstetic;
+exports.surreal = surreal;
+exports.version = require("./package.json").version;
